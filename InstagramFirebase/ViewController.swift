e409795db88c53d876d2b6a8f8ab214df6f6e6a5
franknfjr <frankdonascimentojr@gmail.com>
2019-04-10 10:11:21 -0300

    exemplo de um input text de email

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -17,6 +17,16 @@ class ViewController: UIViewController {
         return button
     }()
     
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,8 +35,16 @@ class ViewController: UIViewController {
         plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        
+        view.addSubview(emailTextField)
+        
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20),
+            emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            emailTextField.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        
     }
-
-
 }
 
