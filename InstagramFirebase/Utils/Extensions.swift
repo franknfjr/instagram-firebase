b05325df2eb57cbbbc1f8734be0ebf9b07599df6
franknfjr <frankdonascimentojr@gmail.com>
2019-04-10 20:37:28 -0300

    criando o contexto util para refatorar o coding das cores

---
+++ InstagramFirebase/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Frank Ferreira on 10/04/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
