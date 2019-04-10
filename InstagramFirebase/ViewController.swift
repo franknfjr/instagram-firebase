8097879222e83b4fa8d14a5a49353b0fa05753ef
franknfjr <frankdonascimentojr@gmail.com>
2019-04-10 09:50:01 -0300

    adicionando botão de upload foto

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let plusPhotoButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
     }
 
 
