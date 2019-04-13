a36df34d91c65b6a341839fef8b547a794c789b0
franknfjr <frankdonascimentojr@gmail.com>
2019-04-13 12:02:16 -0300

    buscando a imagem do user e exibido no header

--- InstagramFirebase/UserProfile/UserProfileHeader.swift
+++ InstagramFirebase/UserProfile/UserProfileHeader.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import Firebase
 
 class UserProfileHeader: UICollectionViewCell {
+    
+    let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        return iv
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .blue
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: self.leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBotton: 0, paddingRight: 0, width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        profileImageView.clipsToBounds = true
+        
+        setupProfileImage()
+    }
+    
+    fileprivate func setupProfileImage(){
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value) { (snapshot) in
+            print(snapshot.value ?? "")
+            
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            
+            guard let profileImageUrl = dictionary["profileImageUrl"] as? String else { return }
+            
+            guard let url = URL(string: profileImageUrl) else { return }
+            
+            URLSession.shared.dataTask(with: url) { (data, response, err) in
+                if let err = err {
+                    //check for the error, then construct the image using data
+                    print("Failed to fetch profile image:", err)
+                    return
+                }
+                
+                //perhaps check for response status of 200 (HTTP OK)
+                guard let data = data else { return }
+                
+                let image = UIImage(data: data)
+                
+                //need to get back into the main UI thread
+                DispatchQueue.main.async {
+                    self.profileImageView.image = image
+                }
+                
+                }.resume()
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
