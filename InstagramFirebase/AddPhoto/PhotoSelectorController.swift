d5ff2e92c6227744e7849ae8597dba796744bf20
franknfjr <frankdonascimentojr@gmail.com>
2019-04-18 09:31:00 -0300

    grid e botoes canceller e next

--- InstagramFirebase/AddPhoto/PhotoSelectorController.swift
+++ InstagramFirebase/AddPhoto/PhotoSelectorController.swift
@@ -8,10 +8,57 @@
 
 import UIKit
 
-class PhotoSelectorController: UICollectionViewController {
+class PhotoSelectorController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .red
+        
+        setupNavigationButtons()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 3) / 4
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    fileprivate func setupNavigationButtons() {
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
+    }
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleNext() {
+        print("handling next")
     }
 }
