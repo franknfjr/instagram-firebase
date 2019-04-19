2611f63c4f215aeb667cdf18da8f034594a498d0
franknfjr <frankdonascimentojr@gmail.com>
2019-04-19 00:42:58 -0300

    model com o struct post e logica pra exibir na tl

---
+++ InstagramFirebase/Models/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  InstagramFirebase
+//
+//  Created by Frank Ferreira on 19/04/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let imageUrl: String
+    
+    init(dictionary: [String: Any]) {
+        self.imageUrl = dictionary["imageUrl"] as? String ?? ""
+    }
+}
