608a3d3ead44cbf761a936655e6f79ae3c8b2a5c
franknfjr <frankdonascimentojr@gmail.com>
2019-04-19 12:55:10 -0300

    custom image view

--- InstagramFirebase/UserProfile/UserProfileHeader.swift
+++ InstagramFirebase/UserProfile/UserProfileHeader.swift
@@ -13,15 +13,16 @@ class UserProfileHeader: UICollectionViewCell {
     
     var user: User? {
         didSet {
-            setupProfileImage()
+            
+            guard let profileImageUrl = user?.profileImageUrl else { return }
+            profileImageView.loadImageView(urlString: profileImageUrl)
             
             usernameLabel.text = user?.username
         }
     }
     
-    let profileImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.backgroundColor = .red
+    let profileImageView: CustomImageView = {
+        let iv = CustomImageView()
         return iv
     }()
     
@@ -155,32 +156,7 @@ class UserProfileHeader: UICollectionViewCell {
         
         bottomDividerView.anchor(top: stackView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBotton: 0, paddingRight: 0, width: 0, height: 0.5)
     }
-    
-    fileprivate func setupProfileImage(){
-        guard let profileImageUrl = user?.profileImageUrl else { return }
         
-        guard let url = URL(string: profileImageUrl) else { return }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            if let err = err {
-                //check for the error, then construct the image using data
-                print("Failed to fetch profile image:", err)
-                return
-            }
-            
-            //perhaps check for response status of 200 (HTTP OK)
-            guard let data = data else { return }
-            
-            let image = UIImage(data: data)
-            
-            //need to get back into the main UI thread
-            DispatchQueue.main.async {
-                self.profileImageView.image = image
-            }
-            
-            }.resume()
-    }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
