43524d35c93d4ce97e5c9480391929003314a740
franknfjr <frankdonascimentojr@gmail.com>
2019-04-20 08:45:13 -0300

    width correto do post

--- InstagramFirebase/Home/HomePostCell.swift
+++ InstagramFirebase/Home/HomePostCell.swift
@@ -19,16 +19,53 @@ class HomePostCell: UICollectionViewCell {
     
     let photoImageView: CustomImageView = {
         let iv = CustomImageView()
-        iv.backgroundColor = .blue
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
         return iv
+    }()
+    
+    let userProfileImageView: CustomImageView = {
+       let iv = CustomImageView()
+        iv.backgroundColor = .blue
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Username"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    let optionsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("•••", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        backgroundColor = .gray
+        
+        addSubview(userProfileImageView)
+        addSubview(usernameLabel)
+        addSubview(optionsButton)
         addSubview(photoImageView)
         
-        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
+        userProfileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBotton: 0, paddingRight: 0, width: 40, height: 40)
+        userProfileImageView.layer.cornerRadius = 40 / 2
+        
+        usernameLabel.anchor(top: topAnchor, left: userProfileImageView.rightAnchor, bottom: photoImageView.topAnchor, right: optionsButton.leftAnchor, paddingTop: 0, paddingLeft: 8, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
+        
+        optionsButton.anchor(top: topAnchor, left: nil, bottom: photoImageView.topAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBotton: 0, paddingRight: 0, width: 44, height: 0)
+        
+        photoImageView.anchor(top: userProfileImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
+        
+        photoImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
