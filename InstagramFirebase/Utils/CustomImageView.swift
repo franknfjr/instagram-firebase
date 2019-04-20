c0a7b6af9116ce2777117ac102322dba211ef25b
franknfjr <frankdonascimentojr@gmail.com>
2019-04-19 23:54:27 -0300

    caching as imagens

--- InstagramFirebase/Utils/CustomImageView.swift
+++ InstagramFirebase/Utils/CustomImageView.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
+var imageCache = [String: UIImage]()
+
 class CustomImageView: UIImageView {
     
     var lastURLUsedToLoadImage: String?
     
     func loadImageView(urlString: String) {
-       
+        
         lastURLUsedToLoadImage = urlString
+        
+        if let cachedImage = imageCache[urlString] {
+            self.image = cachedImage
+            return
+        }
         
         guard let url = URL(string: urlString) else { return }
         
@@ -31,6 +38,8 @@ class CustomImageView: UIImageView {
             
             guard let imageData = data else { return }
             let photoImage = UIImage(data: imageData)
+            
+            imageCache[url.absoluteString] = photoImage
             
             DispatchQueue.main.async {
                 self.image = photoImage
