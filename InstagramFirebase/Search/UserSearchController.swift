9c00c339ee8fd3648760527901d864fdd04a6399
franknfjr <frankdonascimentojr@gmail.com>
2019-04-23 19:58:07 -0300

    search bar ui

--- InstagramFirebase/Search/UserSearchController.swift
+++ InstagramFirebase/Search/UserSearchController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class UserSearchController: UICollectionViewController {
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter username"
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        return sb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .red
+        collectionView?.backgroundColor = .white
+        
+        navigationController?.navigationBar.addSubview(searchBar)
+        
+        let navBar = navigationController?.navigationBar
+        
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBotton: 0, paddingRight: 8, width: 0, height: 0)
     }
 }
