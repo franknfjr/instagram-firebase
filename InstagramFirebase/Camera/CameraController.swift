8db1222ad75d8efe34aa31d4c0b193bd4258d96d
franknfjr <frankdonascimentojr@gmail.com>
2019-04-30 21:33:59 -0300

    configuração para acessar a camera do iPhone

---
+++ InstagramFirebase/Camera/CameraController.swift
@@ -0,0 +1,85 @@
+//
+//  CameraController.swift
+//  InstagramFirebase
+//
+//  Created by Frank Ferreira on 30/04/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraController: UIViewController {
+    
+    let capturePhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "capture_photo"), for: .normal)
+        button.addTarget(self, action: #selector(handleCapturePhoto), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    let dismissButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "right_arrow_shadow"), for: .normal)
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCaptureSession()
+        
+        setupHUD()
+    }
+    
+    fileprivate func setupHUD() {
+        view.addSubview(capturePhotoButton)
+        capturePhotoButton.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 0, paddingBotton: 32, paddingRight: 0, width: 80, height: 80)
+        
+        capturePhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(dismissButton)
+        dismissButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 12, paddingLeft: 0, paddingBotton: 0, paddingRight: 12, width: 50, height: 50)
+    }
+    
+    @objc fileprivate func handleCapturePhoto() {
+        print("Capturing photo...")
+    }
+    
+    @objc fileprivate func handleDismiss() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func setupCaptureSession() {
+        let captureSession = AVCaptureSession()
+        
+        //1.setup inputs
+        guard let caputreDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        do {
+            let input = try AVCaptureDeviceInput(device: caputreDevice)
+            
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            
+        } catch let err {
+            print("Could not setup camera input:", err)
+        }
+        
+        //2.setup outputs
+        let output = AVCapturePhotoOutput()
+        if captureSession.canAddOutput(output) {
+            captureSession.addOutput(output)
+        }
+        
+        //3.setup output prview
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+}
