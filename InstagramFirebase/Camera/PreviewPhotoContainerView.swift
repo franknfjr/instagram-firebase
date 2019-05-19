f00e81453bb31b45d68f156a92ef61032e8c3e1e
franknfjr <frankdonascimentojr@gmail.com>
2019-05-19 11:08:53 -0300

    foto preview

---
+++ InstagramFirebase/Camera/PreviewPhotoContainerView.swift
@@ -0,0 +1,20 @@
+//
+//  PreviewPhotoContainerView.swift
+//  InstagramFirebase
+//
+//  Created by Frank Ferreira on 19/05/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class PreviewPhotoContainerView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
