6c40b2a132dbb881c634abca4d0aaa59a0ac0c62
franknfjr <frankdonascimentojr@gmail.com>
2019-05-29 18:34:10 -0300

    basic template

--- InstagramFirebase/Home/CommentsController.swift
+++ InstagramFirebase/Home/CommentsController.swift
@@ -12,6 +12,8 @@ class CommentsController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Comments"
+        
         collectionView?.backgroundColor = .red
         
     }
@@ -32,7 +34,20 @@ class CommentsController: UICollectionViewController {
         get {
             let containerView = UIView()
             containerView.backgroundColor = .white
-            containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+            containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 75)
+            
+            let submitButton = UIButton(type: .system)
+            submitButton.setTitle("Submit", for: .normal)
+            submitButton.setTitleColor(.black, for: .normal)
+            submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+            
+            containerView.addSubview(submitButton)
+            submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBotton: 0, paddingRight: 12, width: 50, height: 0)
+            
+            let textField = UITextField()
+            textField.placeholder = "Enter Comment"
+            containerView.addSubview(textField)
+            textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
             
             return containerView
         }
