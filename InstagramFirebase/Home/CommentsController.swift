fb05e3c58a76eaf6a3dbf7d72313daac6f0d1cc4
franknfjr <frankdonascimentojr@gmail.com>
2019-05-29 18:54:26 -0300

    handle comment ok

--- InstagramFirebase/Home/CommentsController.swift
+++ InstagramFirebase/Home/CommentsController.swift
@@ -30,26 +30,30 @@ class CommentsController: UICollectionViewController {
         tabBarController?.tabBar.isHidden = false
     }
     
+    var cointainerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 75)
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        containerView.addSubview(submitButton)
+        submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBotton: 0, paddingRight: 12, width: 50, height: 0)
+        
+        let textField = UITextField()
+        textField.placeholder = "Enter Comment"
+        containerView.addSubview(textField)
+        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
+        
+        return containerView
+    }()
+    
     override var inputAccessoryView: UIView? {
         get {
-            let containerView = UIView()
-            containerView.backgroundColor = .white
-            containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 75)
-            
-            let submitButton = UIButton(type: .system)
-            submitButton.setTitle("Submit", for: .normal)
-            submitButton.setTitleColor(.black, for: .normal)
-            submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-            
-            containerView.addSubview(submitButton)
-            submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBotton: 0, paddingRight: 12, width: 50, height: 0)
-            
-            let textField = UITextField()
-            textField.placeholder = "Enter Comment"
-            containerView.addSubview(textField)
-            textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
-            
-            return containerView
+            return cointainerView
         }
     }
     
