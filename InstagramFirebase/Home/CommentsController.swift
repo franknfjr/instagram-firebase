50c653c065aa820f194a4e957ac5b35dafb5aca6
franknfjr <frankdonascimentojr@gmail.com>
2019-05-29 20:26:18 -0300

    lazy var para adicionar o self fora do contexto

--- InstagramFirebase/Home/CommentsController.swift
+++ InstagramFirebase/Home/CommentsController.swift
@@ -30,7 +30,13 @@ class CommentsController: UICollectionViewController {
         tabBarController?.tabBar.isHidden = false
     }
     
-    var cointainerView: UIView = {
+    let commentTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Comment"
+        return textField
+    }()
+    
+    lazy var cointainerView: UIView = {
         let containerView = UIView()
         containerView.backgroundColor = .white
         containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 75)
@@ -39,17 +45,20 @@ class CommentsController: UICollectionViewController {
         submitButton.setTitle("Submit", for: .normal)
         submitButton.setTitleColor(.black, for: .normal)
         submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
         
         containerView.addSubview(submitButton)
         submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBotton: 0, paddingRight: 12, width: 50, height: 0)
         
-        let textField = UITextField()
-        textField.placeholder = "Enter Comment"
-        containerView.addSubview(textField)
-        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
+        containerView.addSubview(self.commentTextField)
+        self.commentTextField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBotton: 0, paddingRight: 0, width: 0, height: 0)
         
         return containerView
     }()
+    
+    @objc func handleSubmit() {
+        print("Inserting comment:", commentTextField.text ?? "")
+    }
     
     override var inputAccessoryView: UIView? {
         get {
